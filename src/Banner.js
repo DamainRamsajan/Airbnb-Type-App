c209782b58431bfa72a73f28c413361b38ae66a7
Damain Ramsajan <dpramsajan@gmail.com>
2021-01-19 04:06:48 +0100

    Date picker setup complete

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,17 @@
 import { Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import "./Banner.css";
+import Search from "./Search"
 
 function Banner() {
+
+    const [showSearch, setShowSearch] = useState (false);
     return (
         <div className = "banner">
             <div className = "banner__search">
-                <Button className = "banner__searchButton" variant = "outlined">Search Dates</Button>
+                
+                <Button onClick = {() => setShowSearch(!showSearch)} className = "banner__searchButton" variant = "outlined">{showSearch ? "Hide" : "Search Dates"}</Button>
+                {showSearch && <Search/>}
             </div>
             <div className = "banner__info">
                 <h1>Get Out and Stretch Your Imagination</h1>
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxrwUmRk8fo0uN3i1vmGfeheA9BnE__Ow",
+    authDomain: "reservations-ecommerce-app.firebaseapp.com",
+    projectId: "reservations-ecommerce-app",
+    storageBucket: "reservations-ecommerce-app.appspot.com",
+    messagingSenderId: "261599534258",
+    appId: "1:261599534258:web:7fbe108ed0f626c69a7dab"
+  };
